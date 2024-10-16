5568eebf14b0203b8988471ea158745e94a1d22b
Alinka Vinokradinka <alinaivanova05@gmail.com>
2024-10-16 16:20:59 +0300

    Готовый проект

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList numbers = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Введите числа (0 для завершения):");
+
+        while (true) {
+            int num = scanner.nextInt();
+            if (num == 0) {
+                break;
+            }
+            if (!numbers.contains(num)) {
+                numbers.add(num);
+            }
         }
+
+        System.out.println("Несовпадающие числа в последовательности:");
+        for (Object number : numbers) {
+            System.out.println(number);
+        }
+
+        scanner.close();
     }
 }
